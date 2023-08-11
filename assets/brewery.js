0444e85b25e323233b49a4fa035967de52c2e938
Evanilla57 <grigg005@cougars.csusm.edu>
2023-08-10 18:59:13 -0700

    renamed id to brewZip

--- assets/brewery.js
+++ assets/brewery.js
@@ -5,59 +5,62 @@ var brewContainerEl = document.querySelector('#brewContainer');
 
 
 var zipSubmitHandler = function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  var zipCode = zipInputEl.value.trim();
+    var zipCode = zipInputEl.value.trim();
 
-  if (zipCode) {
-    getBrew(zipCode);
-//may need to add different text
-    brewContainerEl.textContent = '';
-    zipInputEl.value = '';
-  } else {
-    alert('Please enter a valid zipcode');
-  }
+    if (zipCode) {
+        getBrew(zipCode);
+        //may need to add different text
+        brewContainerEl.textContent = '';
+        zipInputEl.value = '';
+    } else {
+        alert('Please enter a valid zipcode');
+    }
 };
 
 var getBrew = function (zipCode) {
-  var apiUrl = 'https://api.openbrewerydb.org/v1/breweries?by_postal=' + zipCode + '&per_page=5';
+    var apiUrl = 'https://api.openbrewerydb.org/v1/breweries?by_postal=' + zipCode + '&per_page=5';
 
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          displayBrews(data, zipCode);
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayBrews(data, zipCode);
+                });
+                //is this doing anything?
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to find breweries');
         });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to find breweries');
-    });
 };
 
+
+
 var displayBrews = function (brewery, searchZip) {
-  if (brewery.length === 0) {
-    brewContainerEl.textContent = 'No breweries found.';
-    return;
-  }
+    if (brewery.length === 0) {
+        brewContainerEl.textContent = 'No breweries found.';
+        return;
+    }
 
-  brewZipEl.textContent = searchZip;
+    brewZipEl.textContent = searchZip;
 
-  for (var i = 0; i < brewery.length; i++) {
+    for (var i = 0; i < brewery.length; i++) {
 
-    var brewName = brewery[i].name
+        var brewName = brewery[i].name
 
-    console.log(brewName);
+        console.log(brewName);
 
-    var locationEl = document.createElement('li');
-    locationEl.classList = 'list-item flex-row justify-space-between align-center';
+        var locationEl = document.createElement('li');
+        locationEl.classList = 'list-item flex-row justify-space-between align-center';
 
-    brewContainerEl.appendChild(locationEl);
-  }
+        brewContainerEl.appendChild(locationEl);
+    }
 };
 
 brewFormEl.addEventListener('submit', zipSubmitHandler);
