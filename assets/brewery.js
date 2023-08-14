78ebddd35e6ac491fdf1129a8713f31e64c0bce5
Evanilla57 <grigg005@cougars.csusm.edu>
2023-08-14 12:12:07 -0700

    added comments

--- assets/brewery.js
+++ assets/brewery.js
@@ -1,9 +1,10 @@
+// Variables targetting HTML id's
 var brewFormEl = document.querySelector('#brewForm');
 var zipInputEl = document.querySelector('#zipInput');
 var zipDisEl = document.querySelector('#zipDisplay');
 var brewContainerEl = document.querySelector('#brewContainer');
 
-
+// Variable for function to accept zipcode data from zipcode form
 var zipSubmitHandler = function (event) {
     event.preventDefault();
 
@@ -18,6 +19,7 @@ var zipSubmitHandler = function (event) {
     }
 };
 
+// Function to use Zipcode data with API for fetch request
 var getBrew = function (zipCode) {
     var apiUrl = 'https://api.openbrewerydb.org/v1/breweries?by_postal=' + zipCode + '&per_page=5';
 
@@ -28,7 +30,7 @@ var getBrew = function (zipCode) {
                 });
 })};
 
-
+// Variable for function to display and append fetched data below the search form
 var displayBrews = function (breweryData, zipSearch) {
     zipDisEl.textContent = zipSearch;
     if (breweryData.length === 0) {
@@ -43,7 +45,6 @@ var displayBrews = function (breweryData, zipSearch) {
         var brewName = breweryData[i].name;
 
         var locationEl = document.createElement('li');
-        locationEl.classList = 'list-item flex-row justify-space-between align-center';
 
         locationEl.textContent = brewName
 
@@ -51,9 +52,10 @@ var displayBrews = function (breweryData, zipSearch) {
     }
 };
 
+// Event Handler for search button
 brewFormEl.addEventListener('submit', zipSubmitHandler);
 
-//function to remove previous children
+// Function to remove previous children before now list elements are created
 function kill() {
     while (brewContainerEl.firstChild) {
         brewContainerEl.removeChild(brewContainerEl.firstChild);
