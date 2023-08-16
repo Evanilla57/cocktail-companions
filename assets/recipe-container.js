833e70591b659315824c61b72869d58da942b91c
WeenoCoding <hernandez17marc@gmail.com>
2023-08-15 18:56:41 -0700

    got drinks to populate on right side of screen and clicks for links

--- assets/recipe-container.js
+++ assets/recipe-container.js
@@ -1,1 +1,24 @@
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+var drinkName = urlParams.get('drink'); // Retrieve the 'drink' parameter from the URL
 
+var recipeContainerEl = document.querySelector('#recipe-container');
+
+// Fetch recipe details using the retrieved drink name
+var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + encodeURIComponent(drinkName);
+
+fetch(apiUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    displayRecipe(data.drinks[0]);
+  });
+
+var displayRecipe = function (drink) {
+  // Assuming 'strInstructions' is a property in the drink object
+  var instructions = drink.strInstructions;
+
+  // Display the instructions in the recipeContainerEl
+  recipeContainerEl.textContent = instructions;
+};
